eda4dee56e7c752f660c14efd8452658e79c66ab
J.Morales <jorge@chatagency.com>
2022-10-04 13:00:26 -0400

    update default validator class and related contract

--- src/DefaultSignatureValidator.php
+++ src/DefaultSignatureValidator.php
@@ -1,17 +1,33 @@
 <?php
+
 namespace ChatAgency\LaravelSignedRequests;
 
 use ChatAgency\LaravelSignedRequests\Contracts\SignatureValidatorInterface;
 
 class DefaultSignatureValidator implements SignatureValidatorInterface
 {
+    /**
+     * Validate the signature
+     *
+     * @param  string $payload
+     * @param  string $type
+     * @param  string $signature
+     * @param  string $secret
+     * @return void
+     */
     public function validate(string $payload, string $type, string $signature, string $secret)
     {
         $algo = config('signed-requests.algorithms.' . $type);
         return $this->typeIsValid($type) && hash_hmac($algo, trim($payload), $secret) === $signature;
     }
 
-    protected function typeIsValid($type)
+    /**
+     * Validate the signature type
+     *
+     * @param  mixed $type
+     * @return void
+     */
+    public function typeIsValid($type)
     {
         return in_array($type, config('signed-requests.types'));
     }
